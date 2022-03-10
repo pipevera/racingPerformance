2f78b03ea4db326298079a52368f601a9eb3ae1e
Felipe Vera <felipevera10@gmail.com>
2022-03-10 16:23:36 -0300

    probando netlify form

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,71 +1,55 @@
 <template>
   <v-container>
-    <template>
+   
       <v-row class="justify-center mt-15">
         <v-col class="col-12 col-md-6">
           <h1 class="mb-10">Formulario de contacto</h1>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
+            <v-form
+              method="POST"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              ref="form"
+              v-model="valid"
+              lazy-validation
+            >
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                :rules="nameRules"
+                label="Nombre"
+                required
+              ></v-text-field>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
 
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
+             
+              <v-textarea
+                v-model="message"
+                solo
+                name="input-7-4"
+                label="Escribe tu comentario"
+              ></v-textarea>
+     
+              <div data-netlify-recaptcha="true"></div>
 
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+              >
+                Enviar
+              </v-btn>
 
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
-  </v-col>
-  </v-row>
-</template>
+             
+            </v-form>
+          </v-col>
+        </v-row>
   </v-container>
 </template>
 <script>
@@ -82,26 +66,14 @@
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
+      message: "",
     }),
 
     methods: {
       validate () {
         this.$refs.form.validate()
       },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+      
     },
   }
 </script>
