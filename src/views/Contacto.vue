2e6020e657b22fd0b10cf94c917fa7960f3a5c70
Felipe Vera <felipevera10@gmail.com>
2022-03-10 20:09:09 -0300

    pruebsasss

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -54,7 +54,7 @@
   </v-container>
 </template>
 <script>
-import axios from "axios"
+/* import axios from "axios" */
   export default {
     data: () => ({
       valid: true,
@@ -95,17 +95,14 @@ import axios from "axios"
         .join("&");
       },
       handleSubmit () {
-      const axiosConfig = {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" }
-      };
-      axios.post(
-        "/contacto",
-        this.encode({
-          "form-name": "contact",
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          'form-name': 'contact',
           ...this.form
         }),
-        axiosConfig
-      ) 
+  })
       /* .then(() => {
         this.$router.push('thanks')
       })
