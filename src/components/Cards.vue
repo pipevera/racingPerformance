4ec0eb84550caa91bb487ec147dd4dec842bdebe
Felipe Vera <felipevera10@gmail.com>
2022-03-16 20:34:37 -0300

    agregado imagen y descripcion a cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,9 +1,10 @@
 <template>
   <v-col xs="12" sm="6" md="6" lg="4">
     <v-card class="mx-auto" max-width="344">
-      <v-img height="200px"></v-img>
-      <v-card-title>{{producto.nombre}}</v-card-title>
-      <v-card-subtitle>{{producto.descripcion}}</v-card-subtitle>
+      <v-img :src="producto.imagen" height="200px"></v-img>
+      <v-card-title class="titulo">{{producto.nombre}}</v-card-title>
+      <v-card-subtitle>{{producto.precio}}</v-card-subtitle>
+      <v-card-text>{{producto.descripcion}}</v-card-text>
       <v-btn color="success" class="" @click="validate">Iniciar</v-btn>   
     </v-card>
   </v-col>    
@@ -21,6 +22,10 @@ export default {
 <style scoped>
 .card {
   margin-top: 3rem;
+}
+
+.titulo {
+  font-size: 1.5em;
 }
 
 </style>
